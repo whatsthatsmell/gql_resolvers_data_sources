6fef1b4299ac3d1d85439082548a4f73e9a9e12e
Joel Palmer <joelpalmer@gmail.com>
2020-09-11 12:55:42 -0500

    tdd: breaking/failing tests on getTeam ds

--- schema/dataSource.js
+++ schema/dataSource.js
@@ -1,6 +1,7 @@
 const got = require('got')
 
 const url = 'https://statsapi.mlb.com/api/v1/teams/119/roster'
+// https://statsapi.mlb.com/api/v1/teams/119/
 
 module.exports = async () => {
   const { body: { roster } } = await got(url, { responseType: 'json' })
--- tests/datasource.test.js
+++ tests/datasource.test.js
@@ -11,4 +11,14 @@ describe('dataSource', () => {
     expect(roster).toEqual(expect.any(Array))
     expect(roster[0].person.fullName).toBe('Adam Kolarek')
   })
+
+  test('a team object is returned', async () => {
+    nock('https://statsapi.mlb.com').get('/api/v1/teams/119')
+      .replyWithFile(200, `${__dirname}/fixtures/team.json`, {
+        'Content-Type': 'application/json'
+      })
+    const team = await ds.getTeam()
+    expect(team).toEqual(expect.any(Object))
+    // expect(roster[0].person.fullName).toBe('Adam Kolarek')
+  })
 })
